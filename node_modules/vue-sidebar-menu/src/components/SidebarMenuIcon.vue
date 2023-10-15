061f5ab5f10e3d0f0402806dff120d1da85d6ee8
(mavin) <ddw6229@naver.com>
2023-10-15 20:43:25 +0900

    node 파일 추가

---
+++ node_modules/vue-sidebar-menu/src/components/SidebarMenuIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <component
+    :is="icon.element ? icon.element : 'i'"
+    class="vsm--icon"
+    :class="typeof icon === 'string' || (icon instanceof String) ? icon : icon.class"
+    aria-hidden="true"
+    v-bind="icon.attributes"
+  >
+    {{ icon.text }}
+  </component>
+</template>
+
+<script>
+export default {
+  compatConfig: { MODE: 3 },
+  name: 'SidebarMenuIcon',
+  props: {
+    icon: {
+      type: [String, Object],
+      default: ''
+    }
+  }
+}
+</script>
